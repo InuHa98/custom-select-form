87834f914bf837e2a286154f548c4a2ad6d2e90f
InuHa <bimbimskim@gmail.com>
2024-06-09 18:08:16 +0700

    Add files via upload

--- src/custom-select-form.js
+++ src/custom-select-form.js
@@ -63,7 +63,8 @@
             search: '<i class="fas fa-search"></i>',
             show: '<i class="fas fa-chevron-up"></i>',
             hidden: '<i class="fas fa-chevron-down"></i>',
-            remove: '<i class="fas fa-times"></i>'
+            remove: '<i class="fas fa-times"></i>',
+            empty: '<svg width="64" height="41" viewBox="0 0 64 41" xmlns="http://www.w3.org/2000/svg"><g transform="translate(0 1)" fill="none" fill-rule="evenodd"><ellipse fill="#f5f5f5" cx="32" cy="33" rx="32" ry="7"></ellipse><g fill-rule="nonzero" stroke="#d9d9d9"><path d="M55 12.76L44.854 1.258C44.367.474 43.656 0 42.907 0H21.093c-.749 0-1.46.474-1.947 1.257L9 12.761V22h46v-9.24z"></path><path d="M41.613 15.931c0-1.605.994-2.93 2.227-2.931H55v18.137C55 33.26 53.68 35 52.05 35h-40.1C10.32 35 9 33.259 9 31.137V13h11.16c1.233 0 2.227 1.323 2.227 2.928v.022c0 1.605 1.005 2.901 2.237 2.901h14.752c1.232 0 2.237-1.308 2.237-2.913v-.007z" fill="#fafafa"></path></g></g></svg>'
         },
 
         _role: {
@@ -76,7 +77,7 @@
 
         _text: {
             empty_selected: 'Please select an item',
-            empty_option: 'No results found',
+            empty_option: 'No data',
             search: 'Search'
         },
 
@@ -265,7 +266,7 @@
                 });
                 html += '</ul>';
             } else {
-                html = '<div class="' + this._class.empty_option + '">' + this._text.empty_option + '</div>';
+                html = '<div class="' + this._class.empty_option + '">' + this._icon.empty + '<div>' + this._text.empty_option + '</div></div>';
             }
             
             option_container.attr(this._data.id_selection, id_selection).html(html);
@@ -280,7 +281,7 @@
 
                 let empty_result = option_container.find('.' + this._class.empty_option);
                 if(!empty_result.length) {
-                    empty_result = $('<div class="' + this._class.empty_option + '">' + this._text.empty_option + '</div>');
+                    empty_result = $('<div class="' + this._class.empty_option + '">' + this._icon.empty + '<div>' + this._text.empty_option + '</div></div>');
                     empty_result.hide();
                     option_container.append(empty_result);
                 }
